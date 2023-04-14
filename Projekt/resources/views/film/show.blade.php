f421e116494e639eca02f3bf3303bd362bf947f4
Karol66 <karolbal66@gmail.com>
2023-04-14 17:23:49 +0200

    creating a panel to view a film

--- Projekt/resources/views/film/show.blade.php
+++ Projekt/resources/views/film/show.blade.php
@@ -6,8 +6,13 @@
 
         <div class="card-body">
         <h5 class="card-title">Name : {{ $film->name }}</h5>
-        <p class="card-text">Address : {{ $film->address }}</p>
-        <p class="card-text">Mobile : {{ $film->mobile }}</p>
+        <p class="card-text">Type : {{ $film->type }}</p>
+        <p class="card-text">Time : {{ $film->time }}</p>
+        <p class="card-text">Relese Date : {{ $film->releseDate }}</p>
+        <p class="card-text">Country : {{ $film->country }}</p>
+        <p class="card-text">Price : {{ $film->price }}</p>
+        <p class="card-text">Number : {{ $film->number }}</p>
+        <p class="card-text">Image : {{ $film->image }}</p>
   </div>
 
     </hr>
