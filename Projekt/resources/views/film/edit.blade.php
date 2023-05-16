57fdb3f97e11cdd03b0d62a6cb37bbf82a16defa
Karol66 <karolbal66@gmail.com>
2023-05-16 21:34:18 +0200

    edit edit panel 2

--- Projekt/resources/views/film/edit.blade.php
+++ Projekt/resources/views/film/edit.blade.php
@@ -1,38 +1,45 @@
 @extends('film.layout')
 @section('content')
-    <div class="card bg-dark">
+    <div>
         <div class="card-header">
             <h3>Edit Film</h3>
         </div>
-        <div class="card-body">
-            <form action="{{ route('film.update', $film->id) }}" method="post" enctype="multipart/form-data">                @csrf
+        <div>
+            <form action="{{ route('film.update', $film->id) }}" method="post" enctype="multipart/form-data">
+                @csrf
                 @method('PATCH')
                 <input type="hidden" name="id" id="id" value="{{ $film->id }}" />
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" name="name" id="name" value="{{ $film->name }}" class="form-control">
+                    <input type="text" name="name" id="name" value="{{ $film->name }}"
+                        class="form-control gray-background">
                 </div>
                 <div class="form-group">
                     <label for="type">Type</label>
-                    <input type="text" name="type" id="type" value="{{ $film->type }}" class="form-control">
+                    <input type="text" name="type" id="type" value="{{ $film->type }}"
+                        class="form-control gray-background">
                 </div>
                 <div class="form-group">
                     <label for="time">Time</label>
-                    <input type="text" name="time" id="time" value="{{ $film->time }}" class="form-control">
+                    <input type="text" name="time" id="time" value="{{ $film->time }}"
+                        class="form-control gray-background">
                 </div>
                 <div class="form-group">
                     <label for="relese_date">Release Date</label>
                     <input type="text" name="relese_date" id="relese_date" value="{{ $film->relese_date }}"
-                        class="form-control">
+                        class="form-control gray-background">
                 </div>
                 <div class="form-group">
                     <label for="country">Country</label>
-                    <input type="text" name="country" id="country" value="{{ $film->country }}" class="form-control">
+                    <input type="text" name="country" id="country" value="{{ $film->country }}"
+                        class="form-control gray-background">
                 </div>
                 <div class="form-group">
                     <label for="price">Price</label>
-                    <input type="text" name="price" id="price" value="{{ $film->price }}" class="form-control">
+                    <input type="text" name="price" id="price" value="{{ $film->price }}"
+                        class="form-control gray-background">
                 </div>
+                <br>
                 <div class="form-group">
                     <label for="image">Image</label>
                     <input type="file" name="image" id="image" class="form-control-file">
