053dabc29d63d9aba0683e40d5dbde9e73668d7c
Karol66 <karolbal66@gmail.com>
2023-05-17 12:03:53 +0200

    edit create panel

--- Projekt/resources/views/film/create.blade.php
+++ Projekt/resources/views/film/create.blade.php
@@ -1,31 +1,45 @@
 @extends('film.layout')
 @section('content')
-    <div class="card">
-        <div class="card-header">Create Film</div>
-        <div class="card-body">
+    <div>
+        <div class="card-header">
+            <h3>Create Film</h3>
+        </div>
+        <div>
 
             <form method="POST" action="{{ route('film.store') }}" enctype="multipart/form-data">
                 @csrf
-
-                <label>Name</label></br>
-                <input type="text" name="name" id="name" class="form-control"></br>
-                <label>Type</label></br>
-                <input type="text" name="type" id="type" class="form-control"></br>
-                <label>Time</label></br>
-                <input type="text" name="time" id="time" class="form-control"></br>
-                <label>Relese Date</label></br>
-                <input type="text" name="relese_date" id="relese_date" class="form-control"></br>
-                <label>Country</label></br>
-                <input type="text" name="country" id="country" class="form-control"></br>
-                <label>Price</label></br>
-                <input type="text" name="price" id="price" class="form-control"></br>
-                <label>Number</label></br>
-                <input type="text" name="number" id="number" class="form-control"></br>
-                <label for="image">Image:</label>
-                <input type="file" name="image" id="image"></br></br>
-
-                <div>
-                    <button type="submit">Upload</button>
+                <div class="form-group">
+                    <label>Name</label></br>
+                    <input type="text" name="name" id="name" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label>Type</label></br>
+                    <input type="text" name="type" id="type" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label>Time</label></br>
+                    <input type="text" name="time" id="time" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label>Release Date</label></br>
+                    <input type="text" name="release_date" id="release_date" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label>Country</label></br>
+                    <input type="text" name="country" id="country" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label>Price</label></br>
+                    <input type="text" name="price" id="price" class="form-control gray-background"></br>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image:</label>
+                    <input type="file" name="image" id="image"></br></br>
+                </div>
+                <div class="form-group">
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-success">Upload</button>
+                    </div>
                 </div>
             </form>
 
