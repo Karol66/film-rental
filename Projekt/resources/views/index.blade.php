4ade9d90ab0e7704830250dd507d3fb537c28a7f
Karol66 <karolbal66@gmail.com>
2023-05-27 13:42:10 +0200

    improving the site's design 3

--- Projekt/resources/views/index.blade.php
+++ Projekt/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wypożyczalnia Filmów</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 
     <style>
@@ -89,7 +89,7 @@
             margin-bottom: 1rem;
         }
 
-        .new-section .card {
+        .films-section .card {
             position: relative;
             width: 250px;
             height: 300px;
@@ -98,11 +98,7 @@
             transition: filter 0.3s;
         }
 
-        .new-section .card:hover img {
-            filter: blur(5px);
-        }
-
-        .new-section .image-container {
+        .films-section .image-container {
             position: relative;
             width: 100%;
             height: 100%;
@@ -110,25 +106,64 @@
             overflow: hidden;
         }
 
-        .new-section .card img {
+        .films-section .card img {
             width: 100%;
             height: 100%;
             object-fit: cover;
+            transition: filter 0.8s;
         }
 
-        .new-section .card .bi-eye {
+        .films-section .card:hover img {
+            filter: blur(5px);
+        }
+
+        .films-section .card .bi-eye {
             display: none;
             position: absolute;
             top: 50%;
             left: 50%;
-            transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%) scale(0.5);
+            opacity: 0;
+            transition: opacity 0.8s, transform 0.8s;
+            transition-delay: 0.2s;
             z-index: 1;
             color: white;
             font-size: 40px;
         }
 
-        .new-section .card:hover .bi-eye {
+        .films-section .card:hover .bi-eye {
             display: block;
+            animation: eyeAppear 0.8s ease forwards;
+        }
+
+        .films-section .card .film-title {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background-color: rgba(0, 0, 0, 0.7);
+            padding: 10px;
+            color: white;
+            text-align: center;
+            opacity: 0;
+            transform: translateY(0%);
+            transition: opacity 0.6s, transform 0.6s;
+        }
+
+        .films-section .card:hover .film-title {
+            opacity: 1;
+        }
+
+        @keyframes eyeAppear {
+            0% {
+                opacity: 0;
+                transform: translate(-50%, -50%) scale(0.5);
+            }
+
+            100% {
+                opacity: 1;
+                transform: translate(-50%, -50%) scale(1);
+            }
         }
     </style>
 
@@ -136,6 +171,8 @@
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Wypożyczalnia Filmów</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -167,20 +204,21 @@
         <a href="#" class="btn btn-primary">Przejdź do kolekcji filmów</a>
     </header>
 
-
+    <br>
+    <br>
 
     <section class="popular container">
         <div class="heading">
             <h2 class="heading-title">Filmy</h2>
         </div>
         <div class="carousel-container">
-            <div id="movieCarousel" class="carousel slide" data-ride="carousel">
+            <div id="movieCarousel" class="carousel slide" data-bs-ride="carousel">
                 <ol class="carousel-indicators">
                     @php
                         $chunkedFilms = $films->chunk(4);
                     @endphp
                     @foreach ($chunkedFilms as $key => $filmGroup)
-                        <li data-target="#movieCarousel" data-slide-to="{{ $key }}"
+                        <li data-bs-target="#movieCarousel" data-bs-slide-to="{{ $key }}"
                             @if ($key == 0) class="active" @endif></li>
                     @endforeach
                 </ol>
@@ -215,17 +253,21 @@
                         </div>
                     @endforeach
                 </div>
-                <a class="carousel-control-prev" href="#movieCarousel" role="button" data-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#movieCarousel"
+                    data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Poprzedni</span>
-                </a>
-                <a class="carousel-control-next" href="#movieCarousel" role="button" data-slide="next">
+                    <span class="visually-hidden">Poprzedni</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#movieCarousel"
+                    data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Następny</span>
-                </a>
+                    <span class="visually-hidden">Następny</span>
+                </button>
             </div>
         </div>
     </section>
+
+
 
     <br>
     <br>
@@ -263,63 +305,59 @@
     <br>
     <br>
 
-    <section class="new-section container">
+    <section class="films-section container">
         <div class="heading">
             <h2 class="heading-title">Nowa sekcja</h2>
         </div>
         <div class="row">
             @foreach ($films as $film)
                 <div class="col-md-3">
-                    <div class="card">
-                        <div class="image-container">
-                            <img src="data:image/jpeg;base64,{{ base64_encode($film->image) }}">
+                    <a href="{{ route('film.show', $film->id) }}">
+                        <div class="card">
+                            <div class="image-container">
+                                <img src="data:image/jpeg;base64,{{ base64_encode($film->image) }}">
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white"
+                                class="bi bi-eye" viewBox="0 0 16 16">
+                                <path
+                                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z">
+                                </path>
+                                <path
+                                    d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z">
+                                </path>
+                            </svg>
+                            <div class="film-title">
+                                {{ $film->name }}
+                            </div>
                         </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white"
-                            class="bi bi-eye" viewBox="0 0 16 16">
-                            <path
-                                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z">
-                            </path>
-                            <path
-                                d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z">
-                            </path>
-                        </svg>
-                    </div>
+                    </a>
                 </div>
             @endforeach
         </div>
     </section>
 
-
-
-
-
-
-
-
-
-    <br>
     <br>
 
-    <section class="popular container" id="popular">
+    <section class="popular container">
         <div class="heading">
-            <h2 class="heading-title">Polecane Przez Urzytkowników</h2>
+            <h2 class="heading-title">Filmy Polecane Przez Urzytkowników</h2>
         </div>
         <div class="carousel-container">
-            <div id="popularMoviesCarousel" class="carousel slide" data-ride="carousel">
+            <div id="movieCarousel2" class="carousel slide" data-bs-ride="carousel">
                 <ol class="carousel-indicators">
                     @php
-                        $chunkedPopularMovies = $films->chunk(4);
+                        $chunkedFilms = $films->chunk(4);
                     @endphp
-                    @foreach ($chunkedPopularMovies as $key => $movieGroup)
-                        <li data-target="#popularMoviesCarousel" data-slide-to="{{ $key }}"
+                    @foreach ($chunkedFilms as $key => $filmGroup)
+                        <li data-bs-target="#movieCarousel2" data-bs-slide-to="{{ $key }}"
                             @if ($key == 0) class="active" @endif></li>
                     @endforeach
                 </ol>
                 <div class="carousel-inner">
-                    @foreach ($chunkedPopularMovies as $key => $movieGroup)
+                    @foreach ($chunkedFilms as $key => $filmGroup)
                         <div class="carousel-item @if ($key == 0) active @endif">
                             <div class="row">
-                                @foreach ($movieGroup as $film)
+                                @foreach ($filmGroup as $film)
                                     <div class="col-md-3">
                                         <a href="{{ route('film.show', $film->id) }}">
                                             <div class="film-card">
@@ -346,26 +384,28 @@
                         </div>
                     @endforeach
                 </div>
-                <a class="carousel-control-prev" href="#popularMoviesCarousel" role="button" data-slide="prev">
+                <button class="carousel-control-prev" type="button" data-bs-target="#movieCarousel2"
+                    data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#popularMoviesCarousel" role="button" data-slide="next">
+                    <span class="visually-hidden">Poprzedni</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#movieCarousel2"
+                    data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                    <span class="visually-hidden">Następny</span>
+                </button>
             </div>
         </div>
     </section>
+
+
+
 
 
     <footer class="bg-dark text-white text-center p-3 mt-5">
         <p>Wypożyczalnia Filmów &copy; 2023. Wszelkie prawa zastrzeżone.</p>
     </footer>
 
-    <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
